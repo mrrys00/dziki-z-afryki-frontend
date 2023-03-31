595de41c841eaf6c82cd9d7d4dea6e1d5c62e2de
Szymon Ryś <rysszymon00@gmail.com>
2023-03-31 20:21:48 +0200

    add typing

--- src/Types/Types.d.ts
+++ src/Types/Types.d.ts
@@ -1,4 +1,23 @@
-export interface UserInputLogin {
+import { type ROLES } from '../Constants/Paths.d'
+
+export interface UserInputRegister {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    indexNumber: int
+}
+
+export interface UserInputAuthentication {
     username: string
     password: string
+}
+
+export interface DecodedUser {
+    email: string
+    role: ROLES
+}
+
+export interface AuthResponse {
+    jwt: string
 }
