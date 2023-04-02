6bee90b2c84e77ebb3ebc2c120354498fa37b7e9
Szymon Ryś <rysszymon00@gmail.com>
2023-04-02 16:45:21 +0200

    *fix deprecated router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -7,15 +8,14 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './Components/Auth/AuthProvider'
 
-ReactDOM.render(
+createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <App />
             </AuthProvider>
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
